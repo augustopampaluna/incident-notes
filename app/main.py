4859ba01d4b4aa46487887503b30498a6f8f6342
augustopampaluna <augustopampaluna@gmail.com>
2026-04-09 18:24:17 +0200

    Add .env + centralize basic settings in config.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from app.api.routes.incidents import router as incidents_router
 from app.api.routes.system import router as system_router
+from app.core.config import settings
 
-app = FastAPI(title="Incident Notes API")
+app = FastAPI(title=settings.app_name)
 
 app.include_router(system_router)
 app.include_router(incidents_router)
